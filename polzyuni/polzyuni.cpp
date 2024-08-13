a98b4187ef2147341cae43c5c143a95a84b4282a
timur <timur@ALOSHA>
2024-08-13 09:24:04 +0100

    Добавьте файлы проекта.

---
+++ polzyuni/polzyuni.cpp
@@ -0,0 +1,29 @@
+﻿
+
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+    unordered_map<int, int> wheels; 
+    
+    for (int i = 0; i < n; ++i) { 
+        int size;
+        cin >> size;
+        wheels[size]++; 
+    }
+
+    int amount_of_wagons = 0;
+    
+    for (const auto& pair : wheels) {
+        amount_of_wagons += pair.second / 4;
+    }
+
+    cout << amount_of_wagons;
+
+    return 0;
+}
+
